cdf79ebb58814775bcf43d5cf8c64527d75de991
Rafe Hatfield <rafe.hatfield@suite101.com>
2011-06-24 13:36:04 -0700

    added basic styling and a stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	
+	# creates a submit button with the given name and a cancel link
+	# accepts 2 arguments - form object and the cancel link name
+	def submit_or_cancel(form, name='Cancel')
+		form.submit + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+	end
+	
 end
