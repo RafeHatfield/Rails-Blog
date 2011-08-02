01eaa6377b428363939c00a55048bb420cd335b7
Rafe Hatfield <rafe.hatfield@gmail.com>
2011-08-01 22:49:42 -0700

    mergining in chapter 09

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,18 +1,10 @@
 class Comment < ActiveRecord::Base
-	
-	belongs_to :article
-	
-	validates :name, :email, :body, :presence => true
-	validate :article_should_be_published
-	
-	# after_create :do_something_to_comments
-	
-	def article_should_be_published
-		errors.add(:article_id, "is not published yet") if article && !article.published?
-	end
-	
-	def do_something_to_comments
-		puts "Someone made a comment!"
-	end
-	
+  belongs_to :article
+  
+  validates :name, :email, :body, :presence => true
+  validate :article_should_be_published
+
+  def article_should_be_published
+    errors.add(:article_id, "is not published yet") if article && !article.published?
+  end  
 end
