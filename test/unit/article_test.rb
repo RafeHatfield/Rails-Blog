42f61ceb0d00511e2c65a8678b4a60e6dcfa85ce
Rafe Hatfield <rafe.hatfield@gmail.com>
2011-08-04 22:26:18 -0700

    create email send feature for comments to author, and send to a friend.  started creating tests and fixtures

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should create article" do
+    article = Article.new
+    
+    article.user = users(:eugene)
+    article.title = "Test Article"
+    article.body = "Test body"
+    
+    assert article.save
   end
 end
