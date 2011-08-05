488eac86f53cb1abde9b2995c56ac825bd2e9799
Rafe Hatfield <rafe.hatfield@gmail.com>
2011-08-05 06:15:15 -0700

    created basic tests

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+
   test "should create article" do
     article = Article.new
     
@@ -11,4 +11,31 @@ class ArticleTest < ActiveSupport::TestCase
     
     assert article.save
   end
+  
+  test "should find article" do
+    article_id = articles(:welcome_to_rails).id
+    assert_nothing_raised { Article.find(article_id) }
+  end
+  
+  test "should update article" do
+    article = articles(:welcome_to_rails)
+    assert article.update_attributes(:title => 'New Title')
+  end
+
+  test "should destroy article" do
+    article = articles(:welcome_to_rails)
+    article.destroy
+    assert_raise(ActiveRecord::RecordNotFound) {Article.find(article.id)}
+  end
+  
+  test "should not create an article without title nor body" do
+    article = Article.new
+    assert !article.valid?
+    assert article.errors[:title].any?
+    assert article.errors[:body].any?
+    assert_equal ["can't be blank"], article.errors[:title]
+    assert_equal ["can't be blank"], article.errors[:body]
+    assert !article.save
+  end
+  
 end
